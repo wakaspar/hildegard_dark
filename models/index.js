bf904c0ae058ce169294ebf3b8359e50fa7bd18a
wakaspar <wakaspar@gmail.com>
2021-01-15 15:51:33 -0600

    attempt to fix db prod error by redefininng mongoose.connec() in models/index.js

--- models/index.js
+++ models/index.js
@@ -1,3 +1,18 @@
 var mongoose = require("mongoose");
-mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/api");
+
+
+// mongoose.connect( );
+
+mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost/api", 
+    { useNewUrlParser: true, useUnifiedTopology: true }
+);
+const connection = mongoose.connection;
+// Database sanity check
+connection.once('open', function() {
+    console.log('<<== MongoDB database connection established successfully ==>>');
+})
+
 module.exports.Project = require("./project");
+
+
