778a8be9e0819a109b4b17223f04d51f6a79ef93
wakaspar <wakaspar@gmail.com>
2021-01-17 14:22:41 -0600

    edits models/index.js to include a connection.on('eror') statement.

--- models/index.js
+++ models/index.js
@@ -1,18 +1,17 @@
 var mongoose = require("mongoose");
 
-
-// mongoose.connect( );
-
+// mongoose.connect():
 mongoose.connect(
-    process.env.MONGODB_URI || "mongodb://localhost/api", 
-    { useNewUrlParser: true, useUnifiedTopology: true }
+    process.env.MONGODB_URI || "mongodb://localhost/api", { 
+        useNewUrlParser: true, 
+        useUnifiedTopology: true,
+    }
 );
 const connection = mongoose.connection;
 // Database sanity check
+connection.on('error', error => console.error(error));
 connection.once('open', function() {
     console.log('<<== MongoDB database connection established successfully ==>>');
-})
+});
 
 module.exports.Project = require("./project");
-
-
